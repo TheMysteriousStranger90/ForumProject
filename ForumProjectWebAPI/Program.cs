c4466037bf0b734f25a5e1f73195afe771a9b0ac
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-03 09:34:42 +0300

    Init...

--- ForumProjectWebAPI/Program.cs
+++ ForumProjectWebAPI/Program.cs
@@ -18,9 +18,6 @@ namespace ForumProjectWebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
