55d1bb3e44075cc82ee9bcc05e02f4fc7e6aa4cf
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-03 15:03:51 +0300

    add CategoryRepository...

---
+++ DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ForumProjectContext context) : base(context)
+        {
+        }
+    }
+}
