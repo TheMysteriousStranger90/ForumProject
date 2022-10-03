552cdcd12b73e6028f2d342fb463b80a977830b7
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-03 17:27:31 +0300

    ...

---
+++ DAL/SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using DAL.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ForumProjectContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider.GetRequiredService<ForumProjectContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            
+            //
+        }
+    }
+}
