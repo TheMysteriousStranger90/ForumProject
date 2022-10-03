9b1805d675db06aa6280142ef85dabd408e719b9
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-03 15:01:35 +0300

    fix...

--- DAL/Entities/Roles/RoleTypes.cs
+++ DAL/Entities/Roles/RoleTypes.cs
@@ -3,7 +3,7 @@
     public static class RoleTypes
     {
         public const string Admin = "Admin";
-        public const string Manager = "Manager";
+        public const string Moderator = "Moderator";
         public const string User = "User";
     }
 }
