dfbe34ff6f52117db039dad2922514c060c9a5e4
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-03 10:24:15 +0300

    add interface IRepository...

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task CreateAsync(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
