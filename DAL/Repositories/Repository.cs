358228c28e7b2073d4d9cdbfaa29c66d699e7a9b
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-03 14:44:57 +0300

    add Repository...

---
+++ DAL/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly ForumProjectContext _context;
+
+        protected Repository(ForumProjectContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id).AsTask();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Update(entity);
+        }
+    }
+}
