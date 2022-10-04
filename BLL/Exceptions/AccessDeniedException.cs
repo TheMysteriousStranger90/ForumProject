304bb581d2d04e92c11e3814c5c4b5838d6402f9
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-04 13:35:26 +0300

    add other exceptions...

---
+++ BLL/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BLL.Exceptions
+{
+    [Serializable]
+    public class AccessDeniedException : ForumProjectException
+    {
+        public AccessDeniedException()
+        {
+        }
+
+        public AccessDeniedException(string message) : base(message)
+        {
+        }
+
+        public AccessDeniedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AccessDeniedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ BLL/Exceptions/NotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BLL.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : ForumProjectException
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
